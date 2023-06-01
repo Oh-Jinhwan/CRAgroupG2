d29f6edf159f8f73c4f753dfddfdd9615ddc4e21
busan.choi <cbc.930709@gmail.com>
2023-06-01 09:54:22 +0900

    GetGop 함수 구현 / Testcase 하나 추가.

--- Project1/cal.cpp
+++ Project1/cal.cpp
@@ -1,3 +1,7 @@
 class Cal {
-
+public:
+	int GetGop(int param1, int param2)
+	{
+		return (param1 * param2);
+	}
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -5,3 +5,12 @@ TEST(TestCaseName, TestName) {
 	Cal* cal = new Cal();
 	EXPECT_TRUE(cal != nullptr);
 }
+
+
+
+
+TEST(CalTest, GetGop) {
+	Cal* cal = new Cal();
+
+	EXPECT_EQ(cal->GetGop(5, 7), 35);
+}
