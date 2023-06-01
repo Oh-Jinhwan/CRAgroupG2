92f6096c6c285f0dd5a1e56971c9dafc8ca4ad43
kwanghun <lighthun@gmail.com>
2023-06-01 09:55:32 +0900

    getMinus Commit

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -5,3 +5,13 @@ TEST(TestCaseName, TestName) {
 	Cal* cal = new Cal();
 	EXPECT_TRUE(cal != nullptr);
 }
+
+TEST(TestCaseName, TestName_MINUS1) {
+	Cal* cal = new Cal();
+	EXPECT_EQ(4,cal->getMinus(10,6));
+}
+
+TEST(TestCaseName, TestName_MINUS21) {
+	Cal* cal = new Cal();
+	EXPECT_EQ(-3, cal->getMinus(2, 5));
+}
